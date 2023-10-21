849d355393986fa3c80f62bcca20099be73837d5
Ommaan <93634534+Ommaan@users.noreply.github.com>
2023-10-21 10:26:19 +0600

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,16 @@
 require("@nomicfoundation/hardhat-toolbox");
+const dotenv = require("dotenv");
+dotenv.config();
 
-/** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.19",
+  solidity: "0.8.4", 
+  networks: {
+    linea: {
+      url: process.env.REACT_APP_LINEA_RPC_URL, // Use the correct environment variable names
+      accounts: [process.env.REACT_APP_PRIVATE_KEY], // Use the correct environment variable names
+    },
+  },
+  etherscan: {
+    apiKey: process.env.REACT_APP_ETHERSCAN_KEY, // Use the correct environment variable name
+  },
 };
